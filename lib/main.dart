546f805a7e954d5d439a62ae28c5386a23f8720b
Saad Bakht <rotk13@gmail.com>
2020-06-14 12:39:44 -0400

    create 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        title: "Magic 8 Ball",
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text("Magic 8 Ball"),
+          ),
+          backgroundColor: Colors.teal,
+          body: Row(
+            children: [
+              Expanded(child: EightBall()),
+              Expanded(child: EightBall()),
+            ],
+          ),
+        ),
       ),
     );
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int state = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            state = Random().nextInt(4) + 1;
+          });
+        },
+        child: Image.asset('images/ball$state.png'),
+      ),
+    );
+  }
+}
